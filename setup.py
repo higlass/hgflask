3d07de714b3a81d0204d537d32872ee843781214
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2018-02-18 17:55:39 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hgflask',
+    version='0.1',
+    description='Portable higlass tile server',
+    author='Nezar Abdennur',
+    author_email='nabdennur@gmail.com',
+    url='',
+    packages=['hgflask'],
+)
